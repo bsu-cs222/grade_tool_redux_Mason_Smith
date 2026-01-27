3ecc25ba41fd1cb002ae23da7a9672537f050694
masonsmith4 <msmithbsu4@gmail.com>
2026-01-27 15:48:29 -0500

    GradeToolApp created and packages imported

--- lib/grade_tool_app.dart
+++ lib/grade_tool_app.dart
@@ -1,8 +1,18 @@
-
-import 'package:flutter/cupertino.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(GradeToolApp));
+  runApp(GradeToolApp());
 }
 
-class GradeToolApp extends StatelessWidget {}
+class GradeToolApp extends StatelessWidget {
+  const GradeToolApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: create
+    )
+  }
+}
