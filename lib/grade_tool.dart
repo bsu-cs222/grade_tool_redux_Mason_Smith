543b3bb37185b81aaa2042cd9a0e84ad86fdcd8f
masonsmith4 <msmithbsu4@gmail.com>
2026-01-27 11:18:22 -0500

    Create Range class and new grade list -- BROKEN

--- lib/grade_tool.dart
+++ lib/grade_tool.dart
@@ -30,3 +30,19 @@ class GradeTool {
     }
   }
 }
+
+class Range {
+  final double minimum;
+  final double maximum;
+  String letterGrade;
+
+  Range (this.minimum, this.maximum, this.letterGrade);
+}
+
+List<Range> gradeRanges = [
+  Range(0.9, 1, "A"),
+  Range(0.8, 0.89, "B"),
+  Range(0.7, 0.79, "C"),
+  Range(0.6, 0.69, "D"),
+  Range(0, 0.59, "F"),
+];
