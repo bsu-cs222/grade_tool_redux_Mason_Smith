9a6b9dde949517bd5392a028ee48455f6e4635cd
masonsmith4 <msmithbsu4@gmail.com>
2026-01-27 11:32:07 -0500

    got rid of unused class code

--- lib/grade_tool.dart
+++ lib/grade_tool.dart
@@ -1,28 +1,3 @@
-enum Grade {
-  A,
-  B,
-  C,
-  D,
-
-  F,
-}
-
-class GradeTool {
-  Grade convert(double grade) {
-    if (grade >= 0.9) {
-      return Grade.A;
-    } else if (grade >= 0.8) {
-      return Grade.B;
-    } else if (grade >= 0.7) {
-      return Grade.C;
-    } else if (grade >= 0.6) {
-      return Grade.D;
-    } else {
-      return Grade.F;
-    }
-  }
-}
-
 class Range {
   final double minimum;
   final double maximum;
